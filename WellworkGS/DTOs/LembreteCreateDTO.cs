0e1c688331330f498ca0bb010c121fb04921a0d1
Henrique Batista <hbshenrique.ti@gmail.com>
2025-11-23 01:13:05 -0300

    Add DTO Lembrete

---
+++ WellworkGS/DTOs/LembreteCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WellworkGS.DTOs;
+
+public class LembreteCreateDTO
+{
+    public int IdUsuario { get; set; }
+    public string TipoLembrete { get; set; }
+    public int? Frequencia { get; set; }
+    public char Ativo { get; set; }
+}
---
+++ WellworkGS/DTOs/LembreteReadDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace WellworkGS.DTOs;
+
+public class LembreteReadDTO
+{
+    public int IdLembrete { get; set; }
+    public int IdUsuario { get; set; }
+    public string TipoLembrete { get; set; }
+    public int? Frequencia { get; set; }
+    public char Ativo { get; set; }
+}
